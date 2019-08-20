7528e8bbb63d0be26058a583224dca116cb9d591
George Farro <gtfarro@gmail.com>
2019-08-20 12:56:52 -0500

    playing with scene shaders

--- src/index.js
+++ src/index.js
@@ -14,5 +14,27 @@ function requireAll (req) { req.keys().forEach(req); }
 requireAll(require.context('./components/', true, /\.js$/));
 
 require('./firebase');
-
 require('./scene.html');
+
+window.addEventListener('load', function() {
+  console.log('All assets are loaded')
+  var leftHand = document.getElementById('lhand');
+var rightHand = document.getElementById('rhand');
+leftHand.addEventListener('collide', function (e) {
+  console.log('Left hand has collided with body #' + e.detail.body.id);
+
+  e.detail.target.el;  // Original entity (playerEl).
+  e.detail.body.el;    // Other entity, which playerEl touched.
+  e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
+  e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+});
+
+rightHand.addEventListener('collide', function (e) {
+  console.log('Right hand has collided with body #' + e.detail.body.id);
+
+  e.detail.target.el;  // Original entity (playerEl).
+  e.detail.body.el;    // Other entity, which playerEl touched.
+  e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
+  e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+});
+})
