4c2a4c73500383f34debf1e0e2f7da40e6c0f16b
Neilson Gamble <neilsongamble@scandy-mac-pro.local>
2019-08-21 09:37:38 -0500

    adding oculus-touch-controls tracked hand controllers and a pallette plane for left hand.

---
+++ src/components/colliderCheck.js
@@ -0,0 +1,9 @@
+AFRAME.registerComponent("collider-check", {
+  dependencies: ["raycaster"],
+
+  init: function() {
+    this.el.addEventListener("raycaster-intersection", function() {
+      console.log("Player hit something!");
+    });
+  }
+});
--- src/index.js
+++ src/index.js
@@ -1,40 +1,43 @@
 // AFRAME first!
-require('aframe');
+require("aframe");
 
-require('aframe-event-set-component');
-require('aframe-particle-system-component');
-require('aframe-physics-system');
-require('aframe-physics-extras');
-require('super-hands');
-require('aframe-extras');
+require("aframe-event-set-component");
+require("aframe-particle-system-component");
+require("aframe-physics-system");
+require("aframe-physics-extras");
+require("super-hands");
+require("aframe-extras");
 
-function requireAll (req) { req.keys().forEach(req); }
+function requireAll(req) {
+  req.keys().forEach(req);
+}
 
 // Require all components.
-requireAll(require.context('./components/', true, /\.js$/));
+requireAll(require.context("./components/", true, /\.js$/));
 
-require('./firebase');
-require('./scene.html');
+require("./firebase");
+// require('./scene.html');
+require("./scene_ng.html");
 
-window.addEventListener('load', function() {
-  console.log('All assets are loaded')
-  var leftHand = document.getElementById('lhand');
-var rightHand = document.getElementById('rhand');
-leftHand.addEventListener('collide', function (e) {
-  console.log('Left hand has collided with body #' + e.detail.body.id);
+window.addEventListener("load", function() {
+  console.log("All assets are loaded");
+  var leftHand = document.getElementById("lhand");
+  var rightHand = document.getElementById("rhand");
+  leftHand.addEventListener("collide", function(e) {
+    console.log("Left hand has collided with body #" + e.detail.body.id);
 
-  e.detail.target.el;  // Original entity (playerEl).
-  e.detail.body.el;    // Other entity, which playerEl touched.
-  e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
-  e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+    e.detail.target.el; // Original entity (playerEl).
+    e.detail.body.el; // Other entity, which playerEl touched.
+    e.detail.contact; // Stats about the collision (CANNON.ContactEquation).
+    e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+  });
+
+  rightHand.addEventListener("collide", function(e) {
+    console.log("Right hand has collided with body #" + e.detail.body.id);
+
+    e.detail.target.el; // Original entity (playerEl).
+    e.detail.body.el; // Other entity, which playerEl touched.
+    e.detail.contact; // Stats about the collision (CANNON.ContactEquation).
+    e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
+  });
 });
-
-rightHand.addEventListener('collide', function (e) {
-  console.log('Right hand has collided with body #' + e.detail.body.id);
-
-  e.detail.target.el;  // Original entity (playerEl).
-  e.detail.body.el;    // Other entity, which playerEl touched.
-  e.detail.contact;    // Stats about the collision (CANNON.ContactEquation).
-  e.detail.contact.ni; // Normal (direction) of the collision (CANNON.Vec3).
-});
-})
