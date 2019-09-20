52a991ad4efe8603c7637a8f688c92221d57059b
Neilson Gamble <neilsongamble@scandy-mac-pro.local>
2019-09-20 16:40:26 -0500

    renaming coordinates to match

--- src/components/buttonListener.js
+++ src/components/buttonListener.js
@@ -1,21 +1,19 @@
 AFRAME.registerComponent("button-listener", {
   init: function() {
     var el = this.el;
+    var rotationCountZ = 1;
     var rotationCountX = 1;
-    var rotationY = 1;
     var x = 0;
     var y = 0;
     var z = 0;
 
     // 90 = right
     // -90 = left
-    var directionDeg;
-    var directionDegUD;
+    var directionDegZ;
+    var directionDegX;
     var direction;
 
     el.addEventListener("axismove", function(evt) {
-      console.log(evt.detail.axis);
-
       var thumbStickDirectionX = evt.detail.axis[0];
       var thumbStickDirectionY = evt.detail.axis[1];
       var thumbStickHasStopped =
@@ -32,16 +30,16 @@ AFRAME.registerComponent("button-listener", {
       if (!thumbStickHasStopped) {
         if (thumbStickDirectionX > 0.75 && thumbStickDirectionX < 1) {
           direction = "right";
-          directionDeg = -90;
+          directionDegZ = -90;
         } else if (thumbStickDirectionX < -0.75 && thumbStickDirectionX > -1) {
           direction = "left";
-          directionDeg = 90;
+          directionDegZ = 90;
         } else if (thumbStickDirectionY < -0.75 && thumbStickDirectionY > -1) {
           direction = "up";
-          directionDegUD = -90;
+          directionDegX = -90;
         } else if (thumbStickDirectionY > 0.75 && thumbStickDirectionY < 1) {
           direction = "down";
-          directionDegUD = 90;
+          directionDegX = 90;
         }
       }
 
@@ -50,7 +48,26 @@ AFRAME.registerComponent("button-listener", {
         (direction === "left" || direction === "right")
       ) {
         var currentZ = evt.target.firstElementChild.getAttribute("rotation").z;
-        z = directionDeg + currentZ;
+        z = directionDegZ + currentZ;
+
+        evt.target.firstElementChild.setAttribute(
+          "animation",
+          "property: rotation; to: " +
+            x +
+            " " +
+            y +
+            " " +
+            z +
+            "; loop: false; dur: 100"
+        );
+
+        rotationCountZ++;
+      } else if (
+        thumbStickHasStopped &&
+        (direction === "up" || direction === "down")
+      ) {
+        var currentX = evt.target.firstElementChild.getAttribute("rotation").x;
+        x = directionDegX + currentX;
 
         evt.target.firstElementChild.setAttribute(
           "animation",
@@ -64,25 +81,6 @@ AFRAME.registerComponent("button-listener", {
         );
 
         rotationCountX++;
-      } else if (
-        thumbStickHasStopped &&
-        (direction === "up" || direction === "down")
-      ) {
-        var currentY = evt.target.firstElementChild.getAttribute("rotation").x;
-        x = directionDegUD + currentY;
-
-        evt.target.firstElementChild.setAttribute(
-          "animation",
-          "property: rotation; to: " +
-            x +
-            " " +
-            y +
-            " " +
-            z +
-            "; loop: false; dur: 100"
-        );
-
-        rotationCountY++;
       }
     });
   }
